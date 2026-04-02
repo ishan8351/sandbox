449c6ccb24dfa77033319c64de036cbcf6170c80
Ishan Gupta <141325549+ishan8351@users.noreply.github.com>
2026-04-02 08:35:01 +0530

    Create somefile.tf

---
+++ somefile.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VULNERABLE BUCKET: Missing compliance policies
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "regradar-patient-data-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.my_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
